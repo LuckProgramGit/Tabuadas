b4bcd858ce48df95382728b2735a5401ee969b47
LuckProgramGit <lucaslucasfreitaslfss@gmail.com>
2025-04-05 12:50:30 -0300

    Tabuada commit

---
+++ src/tabuadageral/Tabuadas.java
@@ -0,0 +1,32 @@
+package tabuadageral;
+
+import java.util.Scanner;
+
+public class Tabuadas {
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+        String encerra;
+        do {
+            System.out.println("Tabuadas");
+            System.out.println();
+
+            System.out.println("Digite o número para a tabuada:");
+            int numero = scanner.nextInt();
+
+            for (int i = 1; i <= 10; i++){
+                int resultado = numero *i;
+                System.out.println("Tabuada do " + numero + " X " + i + " é: " + resultado);
+            }
+
+            System.out.println("\n Deseja saber outra tabuada? (sim/não)");
+            scanner.nextLine();
+            encerra = scanner.nextLine().toLowerCase();
+
+        } while (encerra.equals("sim"));
+            System.out.println("Encerrando");
+
+
+
+        scanner.close();
+    }
+}
